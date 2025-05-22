28079ea3bdf60cf0bb1566a3b4a5e322b3e2dff9
Jad <jad.feitrouni@telostouch.com>
2025-05-22 09:29:16 -0400

    fix: add api prefix and fix cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {
+		cors: {
+			origin: '*', // Allows requests from any origin
+			methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specifies allowed HTTP methods
+			preflightContinue: false,
+			optionsSuccessStatus: 204,
+			credentials: true, // Allows sending cookies and authorization headers
+			allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With'
+		}
+	});
+	app.setGlobalPrefix('api');
 	await app.listen(3001);
 }
 bootstrap();
