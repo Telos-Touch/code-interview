77940bf441490727ff9922b9204824a7e78c994e
Jad <jad.feitrouni@telostouch.com>
2025-05-22 09:28:27 -0400

    feat: add workflows page and remove default layout

---
+++ apps/frontend/src/app/workflows/page.tsx
@@ -0,0 +1,5 @@
+import WorkflowList from '@/features/workflow/WorflowList';
+
+export default function WorkflowListPage() {
+	return <WorkflowList />;
+}
---
+++ apps/frontend/src/features/workflow/WorflowList.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+const WorkflowList = () => {
+	// TODO: Fetch the list of workflows from the server and display them
+	return <></>;
+};
+
+export default WorkflowList;
